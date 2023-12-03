d717fa256e5461dabc125eae9ea962acd87b6253
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2023-12-03 18:14:42 -0500

    ok

--- Assets/Characters/Scripts/npcinteraction.cs
+++ Assets/Characters/Scripts/npcinteraction.cs
@@ -102,7 +102,7 @@ public class npcinteraction : MonoBehaviour
         cup.transform.SetParent(transform);
 
         // Adjust the local position and rotation of the cup as needed
-        cup.transform.localPosition = new Vector3(0, 1, 0); // Adjust this as needed
+        cup.transform.localPosition = new Vector3(0, 0, 0); // Adjust this as needed
         cup.transform.localRotation = Quaternion.identity;
     }
 
