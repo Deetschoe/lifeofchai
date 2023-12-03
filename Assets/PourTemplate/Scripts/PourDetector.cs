76367f3f611aa6632cc310bbaa2859a5b1551071
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2023-12-02 19:05:23 -0500

    ok

--- Assets/PourTemplate/Scripts/PourDetector.cs
+++ Assets/PourTemplate/Scripts/PourDetector.cs
@@ -3,5 +3,46 @@ using UnityEngine;
 
 public class PourDetector : MonoBehaviour
 {
+    public int pourThreshold = 45;
+    public Transform origin = null;
 
+    public GameObject streamPrefab = null;
+
+    private bool isPouring = false;
+    private Stream currentStream = null;
+
+    private void Update()
+    {
+        bool pourCheck = CalculatePourAngle() < pourThreshold;
+
+        if (isPouring != pourCheck) {
+            isPouring = pourCheck;
+
+            if (isPouring) {
+                StartPour();
+            }
+            else {
+                EndPour();
+            }
+        }
+    }
+    private void StartPour()
+    {
+        print("Start");
+    }
+    private void EndPour()
+    {
+        print("End");
+    }
+    private float CalculatePourAngle()
+    {
+        //Typically use transform.up
+        //Turns it from degrees to degrees 
+        return transform.forward.y * Mathf.Rad2Deg;
+    }
+    private Stream CreateStream()
+    {
+        GameObject streamObject = Instantiate(streamPrefab, origin.position, Quaternion.identity, transform);
+        return streamObject.GetComponent<Stream>();
+    }
 }
