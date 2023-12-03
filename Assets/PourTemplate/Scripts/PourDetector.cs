0271818a31a36f62abf62c8f7b631237b2291dc1
Deet <Deetschoening@gmail.com>
2023-12-02 23:47:33 -0500

    pouring sounds

--- Assets/PourTemplate/Scripts/PourDetector.cs
+++ Assets/PourTemplate/Scripts/PourDetector.cs
@@ -5,11 +5,19 @@ public class PourDetector : MonoBehaviour
 {
     public int pourThreshold = 45;
     public Transform origin = null;
-
     public GameObject streamPrefab = null;
 
     private bool isPouring = false;
     private Stream currentStream = null;
+
+    // Reference to the AudioSource component
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        // Get the AudioSource component
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void Update()
     {
@@ -30,26 +38,40 @@ public class PourDetector : MonoBehaviour
             }
         }
     }
+
     private void StartPour()
     {
         print("Start");
 
         currentStream = CreateStream();
         currentStream.Begin();
+
+        // Play the audio when pouring starts
+        if (audioSource != null && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
     }
+
     private void EndPour()
     {
         print("End");
 
         currentStream.End();
         currentStream = null;
+
+        // Stop the audio when pouring ends
+        if (audioSource != null && audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
     }
+
     private float CalculatePourAngle()
     {
-        //Typically use transform.up
-        //Turns it from degrees to degrees 
         return transform.up.y * Mathf.Rad2Deg;
     }
+
     private Stream CreateStream()
     {
         GameObject streamObject = Instantiate(streamPrefab, origin.position, Quaternion.identity, transform);
