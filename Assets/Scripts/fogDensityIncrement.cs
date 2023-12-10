114d009b2f378177d8c1860fc06d258d8d9373d3
Deet <Deetschoening@gmail.com>
2023-12-10 10:45:29 -0500

    fog increase

--- Assets/Scripts/fogDensityIncrement.cs
+++ Assets/Scripts/fogDensityIncrement.cs
@@ -1,32 +1,62 @@
 using UnityEngine;
 using System.Collections;
 
-public class FogController : MonoBehaviour
+public class EnvironmentAndParticleSystemController : MonoBehaviour
 {
+    // Fog settings
     public float targetFogDensity = 0.1f; // The target density of the fog
-    public float duration = 45.0f; // Duration over which the fog density increases
+    public float fogDuration = 45.0f; // Duration over which the fog density increases
+
+    // Particle System settings
+    public ParticleSystem targetParticleSystem; // Reference to your particle system
+    public float maxStartLifetime = 5.0f; // The target start lifetime for the particles
+    public int maxParticles = 1000; // Maximum number of particles
+    public float maxSimulationSpeed = 100f; // Maximum simulation speed
+
+    private float initialFogDensity;
+    private float initialStartLifetime;
+    private int initialMaxParticles;
+    private float initialSimulationSpeed;
 
     void Start()
     {
-        // Set initial fog settings
+        // Initialize fog settings
         RenderSettings.fog = true;
         RenderSettings.fogMode = FogMode.ExponentialSquared;
         RenderSettings.fogColor = Color.gray;
-        RenderSettings.fogDensity = 0.10f; // Set your preferred starting fog density
+        RenderSettings.fogDensity = 0.02f; // Set your preferred starting fog density
 
-        // Start the coroutine to increase fog density
-        StartCoroutine(IncreaseFogDensity());
+        if (targetParticleSystem != null)
+        {
+            var mainModule = targetParticleSystem.main;
+            initialStartLifetime = mainModule.startLifetime.constant;
+            initialMaxParticles = mainModule.maxParticles;
+            initialSimulationSpeed = mainModule.simulationSpeed;
+        }
+
+        // Start the coroutine to increase fog density and adjust particle system
+        StartCoroutine(IncreaseFogDensityAndAdjustParticleSystem());
     }
 
-    IEnumerator IncreaseFogDensity()
+    IEnumerator IncreaseFogDensityAndAdjustParticleSystem()
     {
-        float startDensity = RenderSettings.fogDensity;
+        initialFogDensity = RenderSettings.fogDensity;
         float elapsedTime = 0;
 
-        while (elapsedTime < duration)
+        while (elapsedTime < fogDuration)
         {
             elapsedTime += Time.deltaTime;
-            RenderSettings.fogDensity = Mathf.Lerp(startDensity, targetFogDensity, elapsedTime / duration);
+            float newDensity = Mathf.Lerp(initialFogDensity, targetFogDensity, elapsedTime / fogDuration);
+            RenderSettings.fogDensity = newDensity;
+
+            if (targetParticleSystem != null)
+            {
+                var mainModule = targetParticleSystem.main;
+                mainModule.startLifetime = Mathf.Lerp(initialStartLifetime, maxStartLifetime, elapsedTime / fogDuration);
+                mainModule.maxParticles = (int)Mathf.Lerp(initialMaxParticles, maxParticles, elapsedTime / fogDuration);
+                mainModule.simulationSpeed = Mathf.Lerp(initialSimulationSpeed, maxSimulationSpeed, elapsedTime / fogDuration);
+            }
+
             yield return null;
         }
     }
