fc119ed19e18ad4344f237649093f3ec89f8f864
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2024-01-28 23:43:02 -0500

    current

--- Assets/SetDefaultLocation.cs
+++ Assets/SetDefaultLocation.cs
@@ -1,21 +1,44 @@
 using UnityEngine;
+using System.Collections; // Required for IEnumerator
 
 public class SetDefaultLocation : MonoBehaviour
 {
+    public MonoBehaviour BallSpawnCreature; // Assign this in the Inspector
+
     // Start is called before the first frame update
     void Start()
     {
-        // Find the game object with the tag "spawnHere"
+        // Start the coroutine
+        StartCoroutine(TeleportAndActivate());
+    }
+
+    IEnumerator TeleportAndActivate()
+    {
         GameObject spawnHereObject = GameObject.FindGameObjectWithTag("spawnHere");
 
-        if (spawnHereObject != null)
+        if (spawnHereObject == null)
         {
-            // Set this object's position to the center of the spawnHere object
+            Debug.LogError("No object found with the tag 'spawnHere'");
+            yield break;
+        }
+
+        float startTime = Time.time;
+
+        // Teleport for the first 5 seconds
+        while (Time.time - startTime < 5)
+        {
             transform.position = spawnHereObject.transform.position;
+            yield return null; // Wait for the next frame
+        }
+
+        // After 5 seconds, enable the BallSpawnCreature script
+        if (BallSpawnCreature != null)
+        {
+            BallSpawnCreature.enabled = true;
         }
         else
         {
-            Debug.LogError("No object found with the tag 'spawnHere'");
+            Debug.LogError("BallSpawnCreature script not assigned!");
         }
     }
 }
